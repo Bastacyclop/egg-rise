86a64f3ab91b993fa70e21105c4dfa0cc30748b0
Thomas Koehler <basta.t.k+git@gmail.com>
2021-02-26 11:25:54 +0000

    implement custom substitution

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use egg::*;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 define_language! {
     enum Rise {
         "var" = Var(Id),
         "app" = App([Id; 2]),
         "lam" = Lambda([Id; 2]),
-        "let" = Let([Id; 3]),
 
         "map" = Map,
 
@@ -43,11 +42,6 @@ impl Analysis<Rise> for RiseAnalysis {
             Rise::Var(v) => {
                 free.insert(*v);
             }
-            Rise::Let([v, a, b]) => {
-                extend(&mut free, b);
-                free.remove(v);
-                extend(&mut free, a);
-            }
             Rise::Lambda([v, a]) => {
                 extend(&mut free, a);
                 free.remove(v);
@@ -64,10 +58,6 @@ fn var(s: &str) -> Var {
     s.parse().unwrap()
 }
 
-fn is_not_same_var(v1: Var, v2: Var) -> impl Fn(&mut RiseEGraph, Id, &Subst) -> bool {
-    move |egraph, _, subst| egraph.find(subst[v1]) != egraph.find(subst[v2])
-}
-
 fn contains_ident(v1: Var, v2: Var) -> impl Fn(&mut RiseEGraph, Id, &Subst) -> bool {
     move |egraph, _, subst| egraph[subst[v1]].data.free.contains(&subst[v2])
 }
@@ -80,58 +70,87 @@ fn rules() -> Vec<Rewrite<Rise, RiseAnalysis>> {
     vec![
         rewrite!("map-fusion";
                  "(app (app map ?f) (app (app map ?g) ?arg))" =>
-                 { withFreshVar("?mfu", "(app (app map (lam ?mfu (app ?f (app ?g (var ?mfu))))) ?arg)") }),
+                 { with_fresh_var("?mfu", "(app (app map (lam ?mfu (app ?f (app ?g (var ?mfu))))) ?arg)") }),
         rewrite!("map-fission";
                  "(app map (lam ?x (app ?f ?gx)))" =>
-                 { withFreshVar("?mfi", "(lam ?mfi (app (app map ?f) (app (app map (lam ?x ?gx)) (var ?mfi))))") }
+                 { with_fresh_var("?mfi", "(lam ?mfi (app (app map ?f) (app (app map (lam ?x ?gx)) (var ?mfi))))") }
                  if neg(contains_ident(var("?f"), var("?x")))),
 
         rewrite!("eta"; "(lam ?v (app ?f (var ?v)))" => "?f"
             if neg(contains_ident(var("?f"), var("?v")))),
-        // subst rules
-        rewrite!("beta";     "(app (lam ?v ?body) ?e)" => "(let ?v ?e ?body)"),
-        rewrite!("let-app";  "(let ?v ?e (app ?a ?b))" => "(app (let ?v ?e ?a) (let ?v ?e ?b))"),
-        rewrite!("let-var-same"; "(let ?v1 ?e (var ?v1))" => "?e"),
-        rewrite!("let-var-diff"; "(let ?v1 ?e (var ?v2))" => "(var ?v2)"
-            if is_not_same_var(var("?v1"), var("?v2"))),
-        rewrite!("let-lam-same"; "(let ?v1 ?e (lam ?v1 ?body))" => "(lam ?v1 ?body)"),
-        rewrite!("let-lam-diff";
-            "(let ?v1 ?e (lam ?v2 ?body))" =>
-            { CaptureAvoid {
-                fresh: var("?fresh"), v2: var("?v2"), e: var("?e"),
-                if_not_free: "(lam ?v2 (let ?v1 ?e ?body))".parse().unwrap(),
-                if_free: "(lam ?fresh (let ?v1 ?e (let ?v2 (var ?fresh) ?body)))".parse().unwrap(),
-            }}
-            if is_not_same_var(var("?v1"), var("?v2"))),
-        rewrite!("let-map"; "(let ?v ?e map)" => "map"),
+        rewrite!("beta"; "(app (lam ?v ?body) ?e)" =>
+                 { BetaApplier { v: var("?v"), e: var("?e"), body: var("?body") } })
     ]
 }
 
-struct CaptureAvoid {
-    fresh: Var,
-    v2: Var,
+struct BetaApplier {
+    v: Var,
     e: Var,
-    if_not_free: Pattern<Rise>,
-    if_free: Pattern<Rise>,
+    body: Var,
 }
 
-impl Applier<Rise, RiseAnalysis> for CaptureAvoid {
+impl Applier<Rise, RiseAnalysis> for BetaApplier {
     fn apply_one(&self, egraph: &mut RiseEGraph, eclass: Id, subst: &Subst) -> Vec<Id> {
-        let e = subst[self.e];
-        let v2 = subst[self.v2];
-        let v2_free_in_e = egraph[e].data.free.contains(&v2);
-        if v2_free_in_e {
-            let mut subst = subst.clone();
-            let sym = Rise::Symbol(format!("_{}", eclass).into());
-            subst.insert(self.fresh, egraph.add(sym));
-            self.if_free.apply_one(egraph, eclass, &subst)
-        } else {
-            self.if_not_free.apply_one(egraph, eclass, &subst)
-        }
+        let new_id = substitute_ident(
+            egraph, subst[self.v], subst[self.e], subst[self.body]);
+        vec![eclass, new_id]
     }
 }
 
-fn withFreshVar(name: &str, pattern: &str) -> MakeFresh {
+// returns a new body where var becomes expr
+fn substitute_ident(egraph: &mut RiseEGraph, var: Id, expr: Id, body: Id) -> Id {
+    struct Env<'a> {
+        egraph: &'a mut RiseEGraph,
+        var: Id,
+        expr: Id,
+        visited: HashMap<Id, Id>
+    }
+
+    fn rec_class(eclass: Id, env: &mut Env) -> Id {
+        match env.visited.get(&eclass).map(|id| *id) {
+            Some(id) => id,
+            None =>
+                if !env.egraph[eclass].data.free.contains(&env.var) {
+                    eclass
+                } else {
+                    let enodes = env.egraph[eclass].nodes.clone();
+                    let new_ids: Vec<_> = {
+                        enodes.into_iter().map(|enode| {
+                            match enode {
+                                Rise::Var(v) if v == env.var => Ok(env.expr),
+                                Rise::Var(_) => Ok(eclass),
+                                Rise::Lambda([v, e]) =>
+                                    if env.egraph[env.expr].data.free.contains(&v) {
+                                        panic!("TODO: capture avoid")
+                                    } else {
+                                        Err(Rise::Lambda([v, rec_class(e, env)]))
+                                    },
+                                Rise::App([f, e]) =>
+                                    Err(Rise::App([rec_class(f, env), rec_class(e, env)])),
+                                _ => panic!("did not expect {:?}", enode)
+                            }
+                        }).collect()
+                    };
+                    let mut new_ids = new_ids.into_iter().map(|x| {
+                        match x {
+                            Ok(id) => id,
+                            Err(enode) => env.egraph.add(enode),
+                        }
+                    }).collect::<Vec<_>>().into_iter();
+                    let first_id = new_ids.next().expect("no new ids");
+                    new_ids.fold(first_id, |a, b| {
+                        let (id, _) = env.egraph.union(a, b);
+                        id
+                    })
+                }
+        }
+    }
+
+    let visited = HashMap::new();
+    rec_class(body, &mut Env { egraph, var, expr, visited })
+}
+
+fn with_fresh_var(name: &str, pattern: &str) -> MakeFresh {
     MakeFresh { 
         prefix: name[1..].into(),
         fresh: var(name),
@@ -158,11 +177,11 @@ trait DSL {
     // f1 >> f2
     fn then<S: Into<String>>(self, other: S) -> String;
     // using patterns for alpha-equivalence
-    fn thenP<S: Into<String>>(self, other: S) -> String;
+    fn then_p<S: Into<String>>(self, other: S) -> String;
 }
 
 static mut COUNTER: u32 = 0;
-fn freshId() -> u32 {
+fn fresh_id() -> u32 {
     unsafe {
         let c = COUNTER;
         COUNTER += 1;
@@ -172,12 +191,12 @@ fn freshId() -> u32 {
 
 impl DSL for String {
     fn then<S: Into<String>>(self, other: S) -> String {
-        let c = freshId();
+        let c = fresh_id();
         format!("(lam x{} (app {} (app {} (var x{}))))", c, other.into(), self, c)
     }
 
-    fn thenP<S: Into<String>>(self, other: S) -> String {
-        let c = freshId();
+    fn then_p<S: Into<String>>(self, other: S) -> String {
+        let c = fresh_id();
         format!("(lam ?x{} (app {} (app {} (var ?x{}))))", c, other.into(), self, c)
     }
 }
@@ -186,18 +205,17 @@ impl DSL for &str {
     fn then<S: Into<String>>(self, other: S) -> String {
         String::from(self).then(other)
     }
-
-    fn thenP<S: Into<String>>(self, other: S) -> String {
-        String::from(self).thenP(other)
+    fn then_p<S: Into<String>>(self, other: S) -> String {
+        String::from(self).then_p(other)
     }
 }
 
-fn prove_equiv(name: &str, startS: String, goalS: String) {
+fn prove_equiv(name: &str, start_s: String, goal_s: String) {
     println!();
     println!("{}", name);
 
-    let start = startS.parse().unwrap();
-    let goal = goalS.parse().unwrap();
+    let start = start_s.parse().unwrap();
+    let goal = goal_s.parse().unwrap();
     println!("starting from {}", start);
 
     let goals: Vec<Pattern<Rise>> = vec![goal];
@@ -232,8 +250,8 @@ fn main() {
         "(app (app padClamp 1) 1)".then(
         "next"
         )),
-        "(app map (app (app padClamp 1) 1))".thenP(
-        "(app (app padClamp 1) 1)".thenP(
+        "(app map (app (app padClamp 1) 1))".then_p(
+        "(app (app padClamp 1) 1)".then_p(
         "next"
         ))
     );
@@ -246,16 +264,16 @@ fn main() {
     prove_equiv("simple map fusion",
         "(app map (var f1))".then("(app map (var f2))").then("(app map (var f3))").then("(app map (var f4))"),
         //"(lam ?x (app (app map (var f4)) (app (app map (var f3)) (app (app map (var f2)) (app (app map (var f1)) (var ?x))))))".into()
-        format!("(app map {})", "(var f1)".thenP("(var f2)")).thenP(format!("(app map {})", "(var f3)".thenP("(var f4)")))
+        format!("(app map {})", "(var f1)".then_p("(var f2)")).then_p(format!("(app map {})", "(var f3)".then_p("(var f4)")))
     );
 
     prove_equiv("simple map fission",
         format!("(app map {})", "(var f1)".then("(var f2)").then("(var f3)").then("(var f4)")),
-        "(app map (var f1))".thenP("(app map (var f2))").thenP("(app map (var f3))").thenP("(app map (var f4))")
+        "(app map (var f1))".then_p("(app map (var f2))").then_p("(app map (var f3))").then_p("(app map (var f4))")
     );
 
     prove_equiv("map fusion + map fission",
         format!("(app map {})", "(var f1)".then("(var f2)").then("(var f3)").then("(var f4)")),
-        format!("(app map {})", "(var f1)".thenP("(var f2)")).thenP(format!("(app map {})", "(var f3)".thenP("(var f4)")))
+        format!("(app map {})", "(var f1)".then_p("(var f2)")).then_p(format!("(app map {})", "(var f3)".then_p("(var f4)")))
     );
 }
