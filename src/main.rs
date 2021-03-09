1861024e4314fed03da6549664b45591ef5f8ad7
Thomas Koehler <basta.t.k+git@gmail.com>
2021-03-09 16:58:33 +0000

    try a different beta reduction technique

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use egg::*;
 use std::collections::{HashSet, HashMap};
+use std::cmp::Ordering;
 
 define_language! {
     enum Rise {
         "var" = Var(Id),
         "app" = App([Id; 2]),
         "lam" = Lambda([Id; 2]),
+
+        ">>" = Then([Id; 2]),
 
         Number(i32),
         Symbol(Symbol),
@@ -17,20 +20,26 @@ type RiseEGraph = EGraph<Rise, RiseAnalysis>;
 #[derive(Default)]
 struct RiseAnalysis;
 
-#[derive(Debug)]
+#[derive(Default, Debug)]
 struct Data {
     free: HashSet<Id>,
+    beta_extract: RecExpr<Rise>,
 }
 
 impl Analysis<Rise> for RiseAnalysis {
     type Data = Data;
 
-    fn merge(&self, to: &mut Data, from: Data) -> bool {
+    fn merge(&self, to: &mut Data, from: Data) -> Option<Ordering> {
         let before_len = to.free.len();
         to.free.extend(from.free);
-        //to.free.retain(|i| from.free.contains(i));
-        let did_change = before_len != to.free.len();
-        did_change
+        let mut did_change = before_len != to.free.len();
+        if !from.beta_extract.as_ref().is_empty() &&
+            (to.beta_extract.as_ref().is_empty() ||
+                to.beta_extract.as_ref().len() > from.beta_extract.as_ref().len()) {
+            to.beta_extract = from.beta_extract;
+            did_change = true;
+        }
+        if did_change { None } else { Some(Ordering::Greater) }
     }
 
     fn make(egraph: &RiseEGraph, enode: &Rise) -> Data {
@@ -50,7 +59,8 @@ impl Analysis<Rise> for RiseAnalysis {
                 enode.for_each(|c| extend(&mut free, &c));
             }
         }
-        Data { free }
+        let beta_extract = enode.to_recexpr(|id| egraph[id].data.beta_extract.as_ref());
+        Data { free, beta_extract }
     }
 }
 
@@ -78,11 +88,19 @@ fn rules(names: &[&str]) -> Vec<Rewrite<Rise, RiseAnalysis>> {
             if neg(contains_ident(var("?f"), var("?x")))),
         // TODO: if conditions should be recursive filters?
 
+        rewrite!("map-fusion-then";
+            "(>> (app map ?f) (app map ?g))" => "(app map (>> ?f ?g))"),
+        rewrite!("map-fission-then";
+            "(app map (>> ?f ?g))" => "(>> (app map ?f) (app map ?g))"),
+        rewrite!("then-assoc-1"; "(>> ?f (>> ?g ?h))" => "(>> (>> ?f ?g) ?h)"),
+        rewrite!("then-assoc-2"; "(>> (>> ?f ?g) ?h)" => "(>> ?f (>> ?g ?h))"),
+
         // reductions
         rewrite!("eta"; "(lam ?v (app ?f (var ?v)))" => "?f"
             if neg(contains_ident(var("?f"), var("?v")))),
         rewrite!("beta"; "(app (lam ?v ?body) ?e)" =>
-            { BetaApplier { v: var("?v"), e: var("?e"), body: var("?body") } }),
+            //{ BetaApplier { v: var("?v"), e: var("?e"), body: var("?body") } }),
+            { BetaExtractApplier { v: var("?v"), e: var("?e"), body: var("?body") } }),
         rewrite!("remove-transpose-pair";
             "(app transpose (app transpose ?x))" => "?x"),
 
@@ -131,11 +149,134 @@ struct BetaApplier {
 }
 
 impl Applier<Rise, RiseAnalysis> for BetaApplier {
-    fn apply_one(&self, egraph: &mut RiseEGraph, eclass: Id, subst: &Subst) -> Vec<Id> {
+    fn apply_one(&self, egraph: &mut RiseEGraph, _eclass: Id, subst: &Subst) -> Vec<Id> {
         let new_id = substitute_ident(
             egraph, subst[self.v], subst[self.e], subst[self.body]);
         vec![new_id]
     }
+}
+
+struct BetaExtractApplier {
+    v: Var,
+    e: Var,
+    body: Var,
+}
+
+impl Applier<Rise, RiseAnalysis> for BetaExtractApplier {
+    fn apply_one(&self, egraph: &mut RiseEGraph, _eclass: Id, subst: &Subst) -> Vec<Id> {
+        let ex_body = &egraph[subst[self.body]].data.beta_extract;
+        let ex_e = &egraph[subst[self.e]].data.beta_extract;
+        let v_sym = {
+            let ns = &egraph[subst[self.v]].nodes;
+            assert!(ns.len() == 1);
+            match &ns[0] {
+                &Rise::Symbol(sym) => sym,
+                n => panic!("expected symbol, found {:?}", n)
+            }
+        };
+        // println!("before subs: {} [{} / {}]", ex_body, ex_e, v_sym);
+        let result = substitute_expr(v_sym, ex_e, ex_body);
+        // println!("after subs:  {}", result);
+        vec![egraph.add_expr(&result)]
+    }
+}
+
+fn substitute_expr(var: Symbol, expr: &RecExpr<Rise>, body: &RecExpr<Rise>) -> RecExpr<Rise> {
+    struct Env<'a> {
+        var: Symbol,
+        expr: &'a RecExpr<Rise>,
+        body: &'a [Rise],
+        result: &'a mut RecExpr<Rise>
+    }
+
+    fn add_expr(to: &mut RecExpr<Rise>, e: &[Rise], id: Id) -> Id {
+        let node = e[usize::from(id)].clone().map_children(|id| add_expr(to, e, id));
+        to.add(node)
+    }
+
+    fn expr_contains(e: &[Rise], var: Symbol) -> bool {
+        for node in e {
+            if node == &Rise::Symbol(var) {
+                return true
+            }
+        }
+        false
+    }
+
+    fn find_fresh_symbol(a: &[Rise], b: &[Rise]) -> Symbol {
+        for i in 0..std::usize::MAX {
+            let s = Symbol::from(format!("s{}", i));
+            if !expr_contains(a, s) && !expr_contains(b, s) {
+                return s;
+            }
+        }
+        panic!("could not find fresh symbol")
+    }
+
+    fn replace_expr(e: &[Rise], id: Id, v: Symbol, v2: Symbol) -> RecExpr<Rise> {
+        fn replace_add(to: &mut RecExpr<Rise>, e: &[Rise], id: Id, v: Symbol, v2: Symbol) -> Id {
+            let node = e[usize::from(id)].clone();
+            let result = match node {
+                Rise::Symbol(v) => Rise::Symbol(v2),
+                _ => node.map_children(|id| replace_add(to, e, id, v, v2))
+            };
+            to.add(result)
+        }
+        let mut result = RecExpr::default();
+        replace_add(&mut result, e, id, v, v2);
+        result
+    }
+
+    fn unwrap_symbol(n: &Rise) -> Symbol {
+        match n {
+            &Rise::Symbol(s) => s,
+            _ => panic!("expected symbol")
+        }
+    }
+
+    fn rec(index: usize, env: &mut Env) -> Id {
+        match &env.body[index] {
+            &Rise::Var(v) if env.body[usize::from(v)] == Rise::Symbol(env.var) =>
+                add_expr(env.result, env.expr.as_ref(), Id::from(env.expr.as_ref().len() - 1)),
+            Rise::Var(_) | Rise::Symbol(_) | Rise::Number(_) =>
+                add_expr(env.result, env.body, Id::from(index)),
+            &Rise::Lambda([v, _]) if env.body[usize::from(v)] == Rise::Symbol(env.var) =>
+                add_expr(env.result, env.body, Id::from(index)),
+            &Rise::Lambda([v, e])
+            if expr_contains(env.expr.as_ref(),
+                             unwrap_symbol(&env.body[usize::from(v)])) =>
+            {
+                let v2 = find_fresh_symbol(env.body, env.expr.as_ref());
+                let e2 = replace_expr(env.body, e,
+                                      unwrap_symbol(&env.body[usize::from(v)]), v2);
+                let mut e3 = substitute_expr(env.var, env.expr, &e2);
+                let ide3 = Id::from(e3.as_ref().len() - 1);
+                let v2e3 = e3.add(Rise::Symbol(v2));
+                e3.add(Rise::Lambda([v2e3, ide3]));
+                add_expr(env.result, e3.as_ref(), Id::from(e3.as_ref().len() - 1))
+            }
+            &Rise::Lambda([v, e]) => {
+                let v2 = rec(usize::from(v), env);
+                let e2 = rec(usize::from(e), env);
+                env.result.add(Rise::Lambda([v, e2]))
+            }
+            &Rise::App([f, e]) => {
+                let f2 = rec(usize::from(f), env);
+                let e2 = rec(usize::from(e), env);
+                env.result.add(Rise::App([f2, e2]))
+            }
+            node => panic!("did not expect {:?}", node)
+        }
+    }
+
+    let mut result = RecExpr::default();
+    rec(body.as_ref().len() - 1, &mut Env {
+        var,
+        expr,
+        body: body.as_ref(),
+        result: &mut result
+    });
+    result
 }
 
 // returns a new body where var becomes expr
@@ -156,19 +297,20 @@ fn substitute_ident(egraph: &mut RiseEGraph, var: Id, expr: Id, body: Id) -> Id 
                 } else {
                     let enodes = env.egraph[eclass].nodes.clone();
                     // add a dummy node to avoid cycles
-                    let dummy = env.egraph.add(Rise::Symbol(format!("_s_{}_{}_{}", eclass, env.var, env.expr).into()));
+                    let dummy = env.egraph.reserve();
+                    // env.egraph.add(Rise::Symbol(format!("_s_{}_{}_{}", eclass, env.var, env.expr).into()));
                     env.visited.insert(eclass, dummy);
                     let final_id = enodes.into_iter().fold(dummy, |current_id, enode| {
                         let new_id = match enode {
-                            Rise::Var(v) if v == env.var => env.expr,
+                            Rise::Var(v) if env.egraph.find(v) == env.egraph.find(env.var) => env.expr,
                             // ignore dummies
-                            Rise::Symbol(s) if s.as_str().starts_with("_") => dummy,
+                            // Rise::Symbol(s) if s.as_str().starts_with("_") => dummy,
                             Rise::Var(_) | Rise::Symbol(_) => {
                                 panic!("{:?}", enode);
                                 // keeping same e-node would merge the new class with previous class
                             }
                             Rise::Lambda([v, e]) =>
-                                if v == env.var {
+                                if env.egraph.find(v) == env.egraph.find(env.var) {
                                     panic!("{:?}", v)
                                     // keeping same e-node would merge the new class with previous class
                                 } else if env.egraph[env.expr].data.free.contains(&v) {
@@ -363,6 +505,10 @@ fn expr_alpha_rename<L, BS, FS>(e: RecExpr<Rise>,
                 rec(f.into(), env);
                 rec(e.into(), env);
             }
+            Rise::Then([f, g]) => {
+                rec(f.into(), env);
+                rec(g.into(), env);
+            }
             Rise::Lambda([x, e]) => {
                 let s = match env.original_vec[usize::from(x)] {
                     Rise::Symbol(s) => s,
@@ -445,11 +591,11 @@ fn prove_equiv(name: &str, start_s: String, goal_s: String, rule_names: &[&str])
             }
         }).run(&rules_to_goal);
     runner.print_report();
-    // runner.egraph.dot().to_svg(format!("/tmp/{}.svg", name)).unwrap();
+    runner.egraph.dot().to_svg(format!("/tmp/{}.svg", name)).unwrap();
     runner.egraph.check_goals(id, &goals);
 }
 
-fn main() {/*
+fn main() {
     let e =
         "(app map (app (app padClamp 1) 1))".then(
         "(app (app padClamp 1) 1)".then(
@@ -469,7 +615,15 @@ fn main() {/*
     prove_equiv("simple map fusion", fissioned.clone(), half_fused.clone(), fission_fusion_rules);
     prove_equiv("simple map fission", fused.clone(), fissioned, fission_fusion_rules);
     prove_equiv("map fission + map fusion", fused, half_fused, fission_fusion_rules);
-*/
+
+    let simpler_half_fused = "(app map (var f2))".then(format!("(app map {})", "(var f3)".then("(var f4)")));
+    let simpler_fused = format!("(app map {})", "(var f2)".then("(var f3)").then("(var f4)"));
+    prove_equiv("simpler map fission + map fusion", simpler_fused, simpler_half_fused, fission_fusion_rules);
+
+    let then_half_fused = "(>> (app map (>> (var f1) (var f2))) (app map (>> (var f3) (var f4))))".into();
+    let then_fused = "(app map (>> (var f1) (>> (var f2) (>> (var f3) (var f4)))))".into();
+    prove_equiv("then map fission + map fusion", then_fused, then_half_fused,
+                &["map-fusion-then", "map-fission-then", "then-assoc-1", "then-assoc-2"]);
 
     let tmp = "(app map (app (app slide 3) 1))".then("(app (app slide 3) 1)").then("(app map transpose)");
     let slide2d_3_1 = tmp.as_str();
@@ -512,9 +666,10 @@ fn main() {/*
                 format!("(app map (app {} weightsV))", dot)).then(
                 format!("(app {} weightsH)", dot2))))
     ));
+
     // FIXME: equivalence not proven when adding unused map fission rule
     prove_equiv("base to scanline half", base.clone(), scanline_half, &[
-        /*"eta", */"beta", "remove-transpose-pair", "map-fusion",/* "map-fission",*/
+        /*"eta", */"beta", "remove-transpose-pair", "map-fusion", "map-fission",
         "slide-before-map", "map-slide-before-transpose", "slide-before-map-map-f",
         "separate-dot-vh-simplified", "separate-dot-hv-simplified"]);
     // beta impl issue?
